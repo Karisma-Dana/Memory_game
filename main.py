9f3c556a0fd5c706ad3bce1994a9434acd4604fd
karisma-dana <karisma.dana2007@gmail.com>
2025-09-19 01:43:18 +0800

    preatyfy the code

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class flip_game:
                     row=row + 1,
                     column=column,
                 )
-                self.button[(row,column)] = board_btn
+                self.button[(row, column)] = board_btn
 
         restart = tk.Button(
             self.root,
@@ -65,53 +65,57 @@ class flip_game:
             background="white",
             foreground=self.color_gray,
             font=("Consolas", 20),
-            command=self.new_game
+            command=self.new_game,
         )
-        restart.grid(row=self.rows+2,column=0,columnspan=self.cols,sticky="we")
-
-
+        restart.grid(row=self.rows + 2, column=0, columnspan=self.cols, sticky="we")
 
     def flip_card(self, row, col):
-        if (row,col) in self.revealed:
+        if (row, col) in self.revealed:
             return
-        if len(self.flipped) == 2 : # wating for the prosess
+        if len(self.flipped) == 2:  # wating for the prosess
             return
-        
-        idx = row * self.cols + col #inddex for back card that realetec to card order
-        btn = self.button[(row,col)]
-        btn.config(text=self.card[idx], background = self.color_yellow, foreground=self.color_blue )
 
-        self.flipped.append((row,col))
+        idx = row * self.cols + col  # inddex for back card that realetec to card order
+        btn = self.button[(row, col)]
+        btn.config(
+            text=self.card[idx],
+            background=self.color_yellow,
+            foreground=self.color_blue,
+        )
+
+        self.flipped.append((row, col))
 
         if len(self.flipped) == 2:
-            self.root.after(1000,self.check_match)
-
-
+            self.root.after(1000, self.check_match)
 
     def check_match(self):
         (r1, c1), (r2, c2) = self.flipped
-        i1, i2 = r1*self.cols+c1, r2*self.cols+c2
+        i1, i2 = r1 * self.cols + c1, r2 * self.cols + c2
 
         if self.card[i1] == self.card[i2]:
-            #match found
-            self.revealed.add((r1,c1))
-            self.revealed.add((r2,c2))
-            self.score[self.current_player] += 1 # give score to the user who flipped the true card
+            # match found
+            self.revealed.add((r1, c1))
+            self.revealed.add((r2, c2))
+            self.score[
+                self.current_player
+            ] += 1  # give score to the user who flipped the true card
         else:
-            self.button[(r1,c1)].config(text="?", background=self.color_gray,foreground="white")
-            self.button[(r2,c2)].config(text="?", background=self.color_gray,foreground="white")
-            
+            self.button[(r1, c1)].config(
+                text="?", background=self.color_gray, foreground="white"
+            )
+            self.button[(r2, c2)].config(
+                text="?", background=self.color_gray, foreground="white"
+            )
 
-            #change player
+            # change player
             self.current_player = 2 if self.current_player == 1 else 1
 
-        self.flipped=[]
+        self.flipped = []
         self.score_label.config(text=self.get_score_text())
 
-        #check if game over 
-        if len(self.revealed) == self.rows * self.cols :
+        # check if game over
+        if len(self.revealed) == self.rows * self.cols:
             self.end_game()
-
 
     def end_game(self):
         if self.score[1] > self.score[2]:
@@ -122,18 +126,18 @@ class flip_game:
             winner = "Its a tie!"
 
         self.game_over = True
-        self.score_label.config(text=F'{winner}', foreground=self.color_blue )
+        self.score_label.config(text=f"{winner}", foreground=self.color_blue)
         return
-        
 
     def new_game(self):
-        for widget in root.winfo_children(): #will destroy all of the window and start  from the beginning
+        for (
+            widget
+        ) in (
+            root.winfo_children()
+        ):  # will destroy all of the window and start  from the beginning
             widget.destroy()
         flip_game(root)
 
-
-
-        
 
 if __name__ == "__main__":
     root = tk.Tk()  # make an window or base user interface
